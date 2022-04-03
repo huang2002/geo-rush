8a6943900ad867f0ed7d8255722fde79a3ec1dd8
hhh <1340545944@qq.com>
2022-04-03 16:23:10 +0800

    extract mainSceneWorld from mainScene

--- src/mainScene.js
+++ src/mainScene.js
@@ -1,151 +1,31 @@
 import { createBomb, recycleBomb } from './bomb.js';
-import { COMMON_ELASTICITY, FRAME_DURATION, COMMON_FRICTION, COMMON_GRAVITY, COMMON_STATIC_FRICTION, engine, MAX_FRAME_COUNT, createRandomizer, SimpleButton, SIMPLE_BUTTON_WIDTH } from './common.js';
+import { engine, SimpleButton, SIMPLE_BUTTON_WIDTH } from './common.js';
+import { bombs, character, CHARACTER_INIT_X, CHARACTER_SIZE, mainSceneWorld, particleGroups, platformPool, platforms, PLATFORM_MIN_Y, updateCamera, updatePlatforms } from './mainSceneWorld.js';
 import { menuScene } from './menuScene.js';
 import { createParticleGroup } from './particles.js';
 
-const SCORE_SCALE = 0.05;
-
-const CHARACTER_SIZE = 40;
-const CHARACTER_INIT_X = engine.width / 2;
 const BOMB_SPAWN_MIN_GAP = 200;
 const BOMB_IMPACT_COEFFICIENT = 9;
 
-const PLATFORM_COUNT = 5;
-const PLATFORM_MIN_Y = engine.height * 0.45;
-const PLATFORM_MAX_Y = engine.height * 0.60;
-const PLATFORM_MIN_WIDTH = 60;
-const PLATFORM_MAX_WIDTH = 120;
-const PLATFORM_MIN_HEIGHT = 80;
-const PLATFORM_MAX_HEIGHT = 120;
-const PLATFORM_MIN_GAP = 5;
-const PLATFORM_MAX_GAP = 60;
-const PLATFORM_COLORS = [
-    '#F00',
-    '#FF0',
-    '#0F0',
-    '#0FF',
-    '#00F',
-    '#F0F',
-];
-
-const platformYRandomizer = createRandomizer();
-const platformWidthRandomizer = createRandomizer();
-const platformHeightRandomizer = createRandomizer();
-const platformColorRandomizer = createRandomizer();
-const platformGapRandomizer = createRandomizer();
+const SCORE_SCALE = 0.05;
 
 let currentScore = 0;
 
-const character = new POM.BodyNode({
-    category: 'character',
-    classNames: ['character'],
-    gravity: COMMON_GRAVITY,
-    friction: COMMON_FRICTION,
-    staticFriction: COMMON_STATIC_FRICTION,
-    elasticity: COMMON_ELASTICITY,
-    vertices: COM.Vertices.createRectangle(
-        CHARACTER_SIZE,
-        CHARACTER_SIZE,
-    ),
-    style: {
-        fillStyle: '#39F',
-        lineWidth: 2,
-    },
-});
+mainSceneWorld.on('afterUpdate', () => {
 
-const platformPool = new HP.Pool({
-    initSize: PLATFORM_COUNT,
-    create: () => (
-        new POM.BodyNode({
-            category: 'platform',
-            classNames: ['platform'],
-            active: false,
-            friction: COMMON_FRICTION,
-            staticFriction: COMMON_STATIC_FRICTION,
-            elasticity: COMMON_ELASTICITY,
-            style: {
-                fillStyle: '#113',
-                lineWidth: 2,
-            },
-        })
-    ),
-    init(platform) {
-        const width = platformWidthRandomizer.integer(
-            PLATFORM_MIN_WIDTH,
-            PLATFORM_MAX_WIDTH,
-        );
-        const height = platformHeightRandomizer.integer(
-            PLATFORM_MIN_HEIGHT,
-            PLATFORM_MAX_HEIGHT,
-        );
-        platform.updateVertices(
-            COM.Vertices.fromArray([
-                0, 0,
-                0, height,
-                width, height,
-                width, 0,
-            ]),
-        );
-        platform.offset.y = platformYRandomizer.integer(
-            PLATFORM_MIN_Y,
-            PLATFORM_MAX_Y,
-        );
-        platform.style.strokeStyle = platformColorRandomizer.choice(
-            PLATFORM_COLORS,
-        );
-    },
-});
+    currentScore = Math.max(
+        currentScore,
+        Math.floor((character.offset.x - CHARACTER_INIT_X) * SCORE_SCALE),
+    );
 
-/**
- * @typedef {ReturnType<platformPool['create']>} Platform
- */
+    scoreText.content = `Score: ${currentScore}`;
 
-/**
- * @type {(import('./bomb').Bomb)[]}
- */
-let bombs = [];
-
-/**
- * @type {Set<import('./particles').ParticleGroup>}
- */
-const particleGroups = new Set();
-
-/**
- * @type {Platform[]}
- */
-let platforms = [];
-
-const updatePlatforms = () => {
-
-    platforms = platforms.filter(platform => {
-        if (platform.bounds.right >= 0) {
-            return true;
-        } else {
-            mainSceneWorld.removeChild(platform);
-            platformPool.push(platform);
-            return false;
-        }
-    });
-
-    for (let i = platforms.length; i < PLATFORM_COUNT; i++) {
-        const gap = platformGapRandomizer.integer(
-            PLATFORM_MIN_GAP,
-            PLATFORM_MAX_GAP,
-        );
-        const nextPlatform = platformPool.pop();
-        const lastPlatform = platforms[platforms.length - 1];
-        if (lastPlatform) {
-            nextPlatform.offset.x = lastPlatform.offset.x
-                + lastPlatform.bounds.width + gap;
-        } else {
-            nextPlatform.offset.x = character.offset.x
-                - (nextPlatform.bounds.width - character.bounds.width) / 2;
-        }
-        platforms.push(nextPlatform);
-        mainSceneWorld.appendChild(nextPlatform);
+    if (character.bounds.bottom >= engine.height) {
+        mainSceneWorld.deactivate();
+        gameOverSection.visible = true;
     }
 
-};
+});
 
 const handleClick = HUtils.throttle(
     BOMB_SPAWN_MIN_GAP,
@@ -219,58 +99,7 @@ const handleClick = HUtils.throttle(
     }
 );
 
-/**
- * @type {POM.WorldNode<any>}
- */
-const mainSceneWorld = new POM.WorldNode({
-    id: 'main-scene-world',
-    stretch: 1,
-    root: engine,
-    frameDuration: FRAME_DURATION,
-    maxFrameCount: MAX_FRAME_COUNT,
-    listeners: {
-
-        beforeUpdate() {
-
-            updatePlatforms();
-
-            updateCamera();
-
-        },
-
-        afterUpdate() {
-
-            bombs = bombs.filter(bomb => {
-                if (bomb.bounds.top <= engine.height) {
-                    return true;
-                } else {
-                    mainSceneWorld.removeChild(bomb);
-                    return false;
-                }
-            });
-
-            currentScore = Math.max(
-                currentScore,
-                Math.floor((character.offset.x - CHARACTER_INIT_X) * SCORE_SCALE),
-            );
-            scoreText.content = `Score: ${currentScore}`;
-
-            if (character.bounds.bottom >= engine.height) {
-                mainSceneWorld.deactivate();
-                gameOverSection.visible = true;
-            }
-
-        },
-
-    },
-});
-
-const updateCamera = () => {
-    mainSceneWorld.offset.x = engine.width / 2 - character.offset.x
-        - character.bounds.width / 2;
-};
-
-const scoreText = new COM.TextNode({
+export const scoreText = new COM.TextNode({
     id: 'score-text',
     stretchX: 1,
     offsetY: 50,
@@ -281,7 +110,7 @@ const scoreText = new COM.TextNode({
     },
 });
 
-const gameOverSection = COM.create(COM.AlignNode, {
+export const gameOverSection = COM.create(COM.AlignNode, {
     offsetY: 130,
     stretch: 1,
     alignX: 'center',
@@ -365,7 +194,7 @@ export const mainScene = COM.create(HE.SceneNode, {
                 event.data.x,
                 event.data.y,
             );
-        },
+        }
 
     },
 }, [
